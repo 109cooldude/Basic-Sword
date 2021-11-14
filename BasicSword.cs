70eb2115d8d801b9a4b6b345eb70612e5cc32063
Nathan Welby <83448244+109cooldude@users.noreply.github.com>
2021-11-14 13:01:13 +0000

    Create BasicSword.cs

---
+++ BasicSword.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ElysiumMod.Items.Weapons.PreBoss
+{
+    public class BasicSword : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("This is a basic sword");
+        }
+
+        public override void SetDefaults()
+        {
+			item.damage = 50; 
+			item.melee = true; 
+			item.width = 40; 
+			item.height = 40; 
+			item.useTime = 20; 
+			item.useAnimation = 20; 
+			item.knockBack = 6; 
+			item.value = Item.buyPrice(gold: 1); 
+			item.rare = ItemRarityID.Green; 
+			item.UseSound = SoundID.Item1; 
+			item.autoReuse = true; 
+			item.crit = 6; 
+			item.useStyle = ItemUseStyleID.SwingThrow;
+		}
+    }
+}
